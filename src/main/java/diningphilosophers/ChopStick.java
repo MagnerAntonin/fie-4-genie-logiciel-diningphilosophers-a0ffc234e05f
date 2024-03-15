813cc6740f7f8d8b8271a591194d30e2e3354ec0
antonin magner <antonin.magner@etud.univ-jfc.fr>
2024-03-15 15:36:26 +0100

    final

--- src/main/java/diningphilosophers/ChopStick.java
+++ src/main/java/diningphilosophers/ChopStick.java
@@ -14,6 +14,7 @@ public class ChopStick {
         myNumber = ++stickCount;
     }
 
+
     public boolean tryTake(int delay) throws InterruptedException {
         if (!iAmFree) {
             boolean locked = lock.tryLock(300, java.util.concurrent.TimeUnit.MILLISECONDS);
